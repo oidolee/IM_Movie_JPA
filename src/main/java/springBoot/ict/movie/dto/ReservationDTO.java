07d21b16e419ba8d25220b9a84645880fcc74b01
jjjjjinn <155942595+jjjjjinn@users.noreply.github.com>
2024-03-11 17:58:36 +0900

    page_1

--- src/main/java/springBoot/ict/movie/dto/ReservationDTO.java
+++ src/main/java/springBoot/ict/movie/dto/ReservationDTO.java
@@ -1,5 +1,14 @@
 package springBoot.ict.movie.dto;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name="IM_Reservation")
+@Data
 public class ReservationDTO {
 
+	
 }
