1af54e11528075d7e46c113c004dfd6fbe348950
jjjjjinn <155942595+jjjjjinn@users.noreply.github.com>
2024-03-13 10:58:37 +0900

    page_1

--- front-end/src/admin/page_1/Admin_Discount.js
+++ front-end/src/admin/page_1/Admin_Discount.js
@@ -1,25 +1,15 @@
-// Test_1.js
-import React, { useState } from 'react';
+import React, { Component } from "react";
 
-function Test_1() {
-    // useState를 사용하여 상태(state) 정의
-    const [count, setCount] = useState(0);
+class Admin_Discount extends Component {
+    render() {
+        return(
+            <div>
+                
 
-    // 클릭 이벤트 핸들러
-    const handleClick = () => {
-        // count 상태 업데이트
-        setCount(prevCount => prevCount + 1);
-    };
+            </div>
+        );
+    }
 
-    return (
-        <div class="container-fluid px-4">
-            test_1
-            {/* 상태(count)를 화면에 표시 */}
-            <p>Count: {count}</p>
-            {/* 버튼 클릭 시 handleClick 함수 호출 */}
-            <button onClick={handleClick}>Increase Count</button>
-        </div>
-    );
 }
 
-export default Test_1;
+export default Admin_Discount;
