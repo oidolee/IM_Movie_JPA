ff5df7ee9c6a21827751bddfdda093951dc5145e
jjjjjinn <155942595+jjjjjinn@users.noreply.github.com>
2024-03-13 17:46:04 +0900

    page_1

--- src/main/java/springBoot/ict/movie/dto/DiscountDTO.java
+++ src/main/java/springBoot/ict/movie/dto/DiscountDTO.java
@@ -8,8 +8,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import lombok.Data;
 
 @Entity
@@ -20,15 +18,9 @@ public class DiscountDTO {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
     @Id
     private int dc_num;
-	@JsonProperty("dc_main_title")
 	private String dc_main_title;
-	@JsonProperty("dc_sub_title")
 	private String dc_sub_title;
-	@JsonProperty("dc_main_img")
 	private String dc_main_img;
-	@JsonProperty("dc_show")
 	private String dc_show;
-	@JsonProperty("dc_sysdate")
 	private Date dc_sysdate;
-	
 }
