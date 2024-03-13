5a7e963e856bff06d92034567d1aeb9fc710c3b6
jjjjjinn <155942595+jjjjjinn@users.noreply.github.com>
2024-03-13 13:25:00 +0900

    page_1

--- front-end/src/admin/page_1/Admin_Discount_List.js
+++ front-end/src/admin/page_1/Admin_Discount_List.js
@@ -1,35 +1,42 @@
 import React, { Component } from "react";
-import {Table, TableHead, TableRow, TableCell, TableBody, Typography, Button} from "@mui/material"
-import style from '../../styles/admin/page_1/Admin_Discount_List.css';
+import {
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Typography,
+  Button,
+} from "@mui/material";
+import style from "../../styles/admin/page_1/Admin_Discount_List.css";
 import ApiService from "../../ApiService";
 
 class Admin_Discount_List extends Component {
-
   constructor(props) {
     super(props);
 
     this.state = {
       lists: [],
-      mseeage: null 
-    }
+      mseeage: null,
+    };
   }
 
   componentDidMount() {
-    this.discountList(); 
+    this.discountList();
   }
 
   // 목록
   discountList = () => {
     ApiService.listDiscount()
-    .then(res => {
-      this.setState({
-        lists: res.data
+      .then((res) => {
+        this.setState({
+          lists: res.data,
+        });
       })
-    })
-    .catch(err => {
-      console.log("listDiscount Error", err)
-    });
-  }
+      .catch((err) => {
+        console.log("listDiscount Error", err);
+      });
+  };
 
   render() {
     return (
@@ -41,13 +48,18 @@ class Admin_Discount_List extends Component {
         </Typography>
         <br />
         <br />
-        <Button variant="contained" color="primary" className="Add_btn" onClick={this.Admin_Discount_Add}>
+        <Button
+          variant="contained"
+          color="primary"
+          className="Add_btn"
+          onClick={this.Admin_Discount_Add}
+        >
           Discount_Add
         </Button>
         <br />
         <br />
-        <Table>
-          <TableHead>
+        <Table className="Discount_List_Content">
+          <TableHead className="Discount_List_Head">
             <TableRow>
               <TableCell>Num</TableCell>
               <TableCell>Main_Title</TableCell>
@@ -60,24 +72,24 @@ class Admin_Discount_List extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {this.state.lists.map(list => 
-                <TableRow key={list.dc_num}>
-                  <TableCell component="th" scope="list">
-                    {list.dc_num}
-                  </TableCell>
-                  <TableCell>{list.dc_main_title}</TableCell>
-                  <TableCell>{list.dc_sub_title}</TableCell>
-                  <TableCell>{list.dc_main_img}</TableCell>
-                  <TableCell>{list.dc_show}</TableCell>
-                  <TableCell>{list.dc_sysdate}</TableCell>
-                  {/* <TableCell onClick={() => this.editDiscount(list.dc_num)}>
+            {this.state.lists.map((list) => (
+              <TableRow key={list.dc_num}>
+                <TableCell component="th" scope="list">
+                  {list.dc_num}
+                </TableCell>
+                <TableCell>{list.dc_main_title}</TableCell>
+                <TableCell>{list.dc_sub_title}</TableCell>
+                <TableCell>{list.dc_main_img}</TableCell>
+                <TableCell>{list.dc_show}</TableCell>
+                <TableCell>{list.dc_sysdate}</TableCell>
+                {/* <TableCell onClick={() => this.editDiscount(list.dc_num)}>
                     <Create />
                   </TableCell>
                   <TableCell onClick={() => this.deleteDiscount(list.dc_num)}>
                     <Delete />
                   </TableCell> */}
-                </TableRow>
-            )}
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
       </div>
