514c91d56de1363e6a9db84da0f5a402dd74a65a
gyooooh <155943403+gyooooh@users.noreply.github.com>
2024-03-16 22:20:21 +0900

    commit

---
+++ src/main/java/springBoot/ict/movie/dto/StoreMapDTO.java
@@ -0,0 +1,35 @@
+package springBoot.ict.movie.dto;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+
+@Entity
+@Table(name="IM_STORE_TICKETMAP")
+@Data
+public class StoreMapDTO {
+
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Id
+    @JsonProperty("ticketmap_no")
+    private int ticketmap_no;
+    // setGift_num 메서드 추가
+    public void setItem_code(int ticketmap_no) {
+        this.ticketmap_no = ticketmap_no;
+    }
+    @JsonProperty("ticketmap_address")
+    private String ticketmap_address;
+    @JsonProperty("ticketmap_name")
+    private String ticketmap_name;
+    @JsonProperty("ticketmap_latitude")
+    private double ticketmap_latitude;
+    @JsonProperty("ticketmap_longitude")
+    private double ticketmap_longitude;
+}
