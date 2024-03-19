4277e73c50ce76db6d3d7a120924cd673daf541f
jjjjjinn <155942595+jjjjjinn@users.noreply.github.com>
2024-03-19 10:02:46 +0900

    page_1

--- IM_Movie(제출용).sql
+++ IM_Movie(제출용).sql
@@ -65,25 +65,22 @@ commit;
 DROP TABLE IM_Seat;
 CREATE TABLE IM_Seat(
     st_id INT AUTO_INCREMENT PRIMARY KEY,
-    theater_id VARCHAR(50) NOT NULL,
-    screen_id VARCHAR(50) NOT NULL,
-    st_seat_group VARCHAR(50) NOT NULL,
-    st_num VARCHAR(50) NOT NULL,
-    st_line_num VARCHAR(50) NOT NULL,
-    st_check CHAR(1) DEFAULT 'n'
+    st_row VARCHAR(50) NOT NULL, -- 행
+    st_column INT NOT NULL, -- 열  
+    st_check CHAR(1) DEFAULT 'n' -- 'n':예약가능, 'r':예약중, 'y':결제 완료 예약불가능
 );
 
 SELECT * FROM IM_Seat;
 
-INSERT INTO IM_Seat(theater_id, screen_id, st_seat_group, st_num, st_line_num, st_check)
-VALUES ('aa', 'aa', 'aa', 'aa', 'aa', 'n');
-
-UPDATE IM_Seat
-SET st_check='y'
-WHERE st_id=1;
-
-DELETE FROM IM_Seat
-SHERE St_id=1;
+INSERT INTO IM_Seat(st_row, st_column) VALUES
+('A', 1), ('A', 2), ('A', 3), ('A', 4), ('A', 5), ('A', 6), ('A', 7), ('A', 8), ('A', 9), ('A', 10), ('A', 11), ('A', 12), ('A', 13), ('A', 14),
+('B', 1), ('B', 2), ('B', 3), ('B', 4), ('B', 5), ('B', 6), ('B', 7), ('B', 8), ('B', 9), ('B', 10), ('B', 11), ('B', 12), ('B', 13), ('B', 14),
+('C', 1), ('C', 2), ('C', 3), ('C', 4), ('C', 5), ('C', 6), ('C', 7), ('C', 8), ('C', 9), ('C', 10), ('C', 11), ('C', 12), ('C', 13), ('C', 14),
+('D', 1), ('D', 2), ('D', 3), ('D', 4), ('D', 5), ('D', 6), ('D', 7), ('D', 8), ('D', 9), ('D', 10), ('D', 11), ('D', 12), ('D', 13), ('D', 14),
+('E', 1), ('E', 2), ('E', 3), ('E', 4), ('E', 5), ('E', 6), ('E', 7), ('E', 8), ('E', 9), ('E', 10), ('E', 11), ('E', 12), ('E', 13), ('E', 14),
+('F', 1), ('F', 2), ('F', 3), ('F', 4), ('F', 5), ('F', 6), ('F', 7), ('F', 8), ('F', 9), ('F', 10), ('F', 11), ('F', 12), ('F', 13), ('F', 14),
+('G', 1), ('G', 2), ('G', 3), ('G', 4), ('G', 5), ('G', 6), ('G', 7), ('G', 8), ('G', 9), ('G', 10), ('G', 11), ('G', 12), ('G', 13), ('G', 14),
+('H', 1), ('H', 2), ('H', 3), ('H', 4), ('H', 5), ('H', 6), ('H', 7), ('H', 8), ('H', 9), ('H', 10), ('H', 11), ('H', 12), ('H', 13), ('H', 14);
 
 commit;
 
