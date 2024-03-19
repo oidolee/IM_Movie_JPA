75784cc8f420ba4d0f012cb3936bb792333fbc5f
jjjjjinn <155942595+jjjjjinn@users.noreply.github.com>
2024-03-19 17:40:54 +0900

    page_1

--- front-end/src/component/page_1/Reservation_Seat.js
+++ front-end/src/component/page_1/Reservation_Seat.js
@@ -153,7 +153,7 @@ const Reservation_Seat = () => {
   };
 
   const handleSeatSelect = (ip_no, lot, seatNumber, status) => {
-    if (canSelectSeat) {
+    if (!canSelectSeat) {
       alert("수량을 선택해야 좌석을 선택할 수 있습니다.");
       return;
     }
