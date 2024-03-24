f1ff592a34838ffb54c299b7da0e3ac5e76a65d9
Geine's world <85022962+oidolee@users.noreply.github.com>
2024-03-24 20:10:24 +0900

    page_1

--- src/main/java/springBoot/ict/movie/handler/OAuth2SuccessHandler.java
+++ src/main/java/springBoot/ict/movie/handler/OAuth2SuccessHandler.java
@@ -6,6 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -20,6 +21,9 @@ public class OAuth2SuccessHandler  extends SimpleUrlAuthenticationSuccessHandler
 	
 	private final UserAuthProvider jwtProvider;
 	
+	@Value("${redirect.url.development}") // 활성 프로파일에 따른 URL 주입
+    private String redirectUrl;
+	
 	@Override
 	public void onAuthenticationSuccess (
 		HttpServletRequest request, 
@@ -31,11 +35,11 @@ public class OAuth2SuccessHandler  extends SimpleUrlAuthenticationSuccessHandler
 		
 		String userId = oAuth2User.getName();
 		String token = jwtProvider.createToken(userId);
+		System.out.println(" onAuthenticationSuccess token 시작 : " + token );
 		
 		
 		
-		response.sendRedirect("http://localhost:3000/auth/oauth-response/" + token + "/3600");
-		
-		
+        response.sendRedirect(redirectUrl + "/auth/oauth-response/" + token + "/3600");
+//		response.sendRedirect("http://localhost:3000/auth/oauth-response/" + token + "/3600");
 	}
 }
