1eb9737269923c743bc3bb1142f7ab25762fde75
Geine's world <85022962+oidolee@users.noreply.github.com>
2024-03-25 08:23:09 +0900

    buil

--- src/main/java/springBoot/ict/movie/config/WebConfig.java
+++ src/main/java/springBoot/ict/movie/config/WebConfig.java
@@ -29,7 +29,7 @@ public class WebConfig {
 		
 		System.out.println("<<< WebConfig-1 >>>");
 		config.setAllowCredentials(true);
-		config.addAllowedOrigin("http://localhost:3000");
+		// config.addAllowedOrigin("http://localhost:3000");
 		config.addAllowedOrigin("http://3.39.155.236:3000");
 		config.setAllowedHeaders(Arrays.asList(
 					HttpHeaders.AUTHORIZATION,
