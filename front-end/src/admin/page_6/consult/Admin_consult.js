d37d98eadeabdc65549899da5cbb0a059db43c62
C.H Kim <ckdgo9409@gmail.com>
2024-03-26 18:23:07 +0900

    수정

--- front-end/src/admin/page_6/consult/Admin_consult.js
+++ front-end/src/admin/page_6/consult/Admin_consult.js
@@ -18,16 +18,31 @@ function MyPage_consult_list_part() {
         }
     }, []);
 
+    const formatDate = (timestamp) => {
+        const date = new Date(timestamp);
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0'); 
+        const day = String(date.getDate()).padStart(2, '0');
+        return `${year}-${month}-${day}`;
+    };
+
     const reloadConsultList = () => {
         ApiService.fetchConsult()
         .then(res => {
-            console.log("test" + res);
-            setConsult(res.data);
+            console.log("test", res.data);
+            const consultData = res.data.map(item => ({
+                ...item,
+                ib_date: formatDate(item.ib_date) 
+            }));
+            setConsult(consultData);
+            console.log('consultData', consultData);
         })
         .catch(err => {
             console.log('reloadConsultList() Error!!', err);
         });
     }
+
+    
 
     
 
