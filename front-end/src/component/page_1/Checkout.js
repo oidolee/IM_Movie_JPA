75ca7df07b1b318f88529d908fb55719435f67f3
jjjjjinn <155942595+jjjjjinn@users.noreply.github.com>
2024-03-27 14:58:56 +0900

    page_1

--- front-end/src/component/page_1/Checkout.js
+++ front-end/src/component/page_1/Checkout.js
@@ -2,8 +2,9 @@ import React, { useEffect, useRef, useState } from "react";
 import { loadPaymentWidget } from "@tosspayments/payment-widget-sdk";
 import { nanoid } from "nanoid";
 import style from "../../styles/page_1/Checkout.css";
-import { useCookies } from 'react-cookie';
-import { jwtDecode } from 'jwt-decode';
+import { useCookies } from "react-cookie";
+import { jwtDecode } from "jwt-decode";
+import ApiService from "../../ApiService";
 
 const clientKey = "test_ck_D5GePWvyJnrK0W0k6q8gLzN97Eoq";
 const customerKey = "YbX2HuSlsC9uVJW6NMRMj";
@@ -12,36 +13,50 @@ const customerKey = "YbX2HuSlsC9uVJW6NMRMj";
 // 시크릿 키 test_sk_ex6BJGQOVDxv2GKPJdBnVW4w2zNb
 // http://localhost:3000/success?paymentType=NORMAL&orderId=xygn4_PUn0O91OCQt1QNf&paymentKey=jvX2KBP9QADpexMgkW36obk1elyyXJVGbR5ozO06yLYlaEJ7&amount=100
 
-
 // searchCutomer : 회원정보조회
-
 
 const App = ({ handleCloseModal }) => {
   const paymentWidgetRef = useRef(null);
   const paymentMethodsWidgetRef = useRef(null);
   const [userEmail, setUserEmail] = useState("");
   const [totalPrice, setTotalPrice] = useState(0); // totalPrice 상태 추가
+  const [customer, setCustomer] = useState([]);
+  const [selectedMovieInfo, setSelectedMovieInfo] = useState(null); // 선택한 영화 정보
 
   useEffect(() => {
-    const authToken = localStorage.getItem("auth_token");
-    if (authToken) {
+    const fetchData = async () => {
+      const authToken = localStorage.getItem("auth_token");
+      if (authToken) {
         const decodedToken = jwtDecode(authToken);
         const user = decodedToken.iss;
         setUserEmail(user);
+        searchCutomer(user);
         console.log("사용자 이메일:", user);
-    }
+      }
 
-     // 로컬 스토리지에서 totalPrice 가져오기
-     const totalPrice = JSON.parse(localStorage.getItem("totalPrice"));
-     if (totalPrice) {
-       setPrice(totalPrice);
-     }
-}, []);
+      const storedTotalPrice = JSON.parse(localStorage.getItem("totalPrice"));
+      if (storedTotalPrice) {
+        setTotalPrice(storedTotalPrice);
+      }
+    };
+
+    fetchData();
+
+    const storedMovieInfo = localStorage.getItem("selectedMovieInfo");
+    if (storedMovieInfo) {
+      try {
+        const parsedMovieInfo = JSON.parse(storedMovieInfo);
+        setSelectedMovieInfo(parsedMovieInfo);
+        console.log(parsedMovieInfo);
+      } catch (error) {
+        console.error("영화 정보를 파싱하는 중 오류 발생:", error);
+      }
+    }
+  }, []);
 
   useEffect(() => {
     const fetchPaymentWidget = async () => {
       const paymentWidget = await loadPaymentWidget(clientKey, customerKey);
-
       const paymentMethodsWidget = paymentWidget.renderPaymentMethods(
         "#payment-widget",
         totalPrice
@@ -67,19 +82,35 @@ const App = ({ handleCloseModal }) => {
     );
   }, [totalPrice]);
 
+  const searchCutomer = (user) => {
+    ApiService.searchCutomer(user)
+      .then((res) => {
+        setCustomer(res.data.dto);
+        console.log("searchCutomer 성공", res.data);
+      })
+      .catch((err) => {
+        console.log("searchCutomer 오류 : ", err);
+      });
+  };
+
   const handlePayment = async () => {
     const paymentWidget = paymentWidgetRef.current;
 
     try {
-      const orderId = nanoid(); // orderId 변수 선언
+      const orderId = nanoid();
       await paymentWidget?.requestPayment({
-        orderId: orderId, // orderId 변수 사용
-        orderName: "티켓",
-        customerName: "사용자",
+        orderId: orderId,
+        orderName: selectedMovieInfo.movie_title,
+        customerName: customer.name,
         customerEmail: userEmail,
-        successUrl: `${window.location.origin}/success?orderId=${orderId}&orderName=티켓&customerEmail=${userEmail}&amount=${totalPrice}`,
-        failUrl: `${window.location.origin}/fail`
+        successUrl: `${window.location.origin}/success?orderId=${orderId}&orderName=${selectedMovieInfo.movie_title}&customerEmail=${userEmail}&amount=${totalPrice}`,
+        failUrl: `${window.location.origin}/fail`,
       });
+      
+      localStorage.setItem("orderId", orderId);
+      localStorage.setItem("selectedMovieTitle", selectedMovieInfo.movie_title);
+      localStorage.setItem("customerEmail", userEmail);
+      localStorage.setItem("totalPrice", totalPrice);
     } catch (err) {
       console.log(err);
     }
